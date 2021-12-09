b5a7c3fe052d0775a77af10519b53bf6e7beb551
aberraxao <diana_milu@live.com.pt>
2021-12-09 16:07:23 +0000

    1.5 added

--- src/Color.java
+++ src/Color.java
@@ -5,29 +5,29 @@
  * rgb[1] - Green
  * rgb[2] - Blue
  */
+
 class Color {
-	
+
 	/**
-	* A1 Commonly used colours
-	**/
-	static final Color RED = new Color(255, 0, 0);
-	static final Color GREEN = new Color(0, 255, 0);
-	static final Color BLUE = new Color(0, 0, 255);
-	static final Color WHITE = new Color(255, 255, 255);
+	 * A1 Commonly used variables & colours
+	 **/
+	static final int MAX = 255;
+	static final Color RED = new Color(Color.MAX, 0, 0);
+	static final Color GREEN = new Color(0, Color.MAX, 0);
+	static final Color BLUE = new Color(0, 0, Color.MAX);
+	static final Color WHITE = new Color(Color.MAX, Color.MAX, Color.MAX);
 	static final Color BLACK = new Color(0, 0, 0);
-	
-	
+
 	private final int[] rgb; // @color
 
 	/**
-	 * Creates an RGB color. Provided values have to 
-	 * be in the interval [0, 255]
+	 * Creates an RGB color. Provided values have to be in the interval [0, 255]
 	 */
 	Color(int r, int g, int b) {
-		if(!valid(r) || !valid(g) || !valid(b))
+		if (!valid(r) || !valid(g) || !valid(b))
 			throw new IllegalArgumentException("invalid RGB values: " + r + ", " + g + ", " + b);
-		
-		this.rgb = new int[] {r, g, b};
+
+		this.rgb = new int[] { r, g, b };
 	}
 
 	/**
@@ -55,22 +55,31 @@ class Color {
 	 * Obtains the luminance in the interval [0, 255].
 	 */
 	int getLuminance() {
-		return (int) Math.round(rgb[0]*.21 + rgb[1]*.71 + rgb[2]*.08);
+		return (int) Math.round(rgb[0] * .21 + rgb[1] * .71 + rgb[2] * .08);
 	}
 
 	static boolean valid(int value) {
 		return value >= 0 && value <= 255;
 	}
-	
+
 	/**
 	 * Checks if the Color A is equal to the Color B
 	 */
-	boolean isEqualTo (Color c) {
+	boolean isEqualTo(Color c) {
 		if (c == null)
 			return false;
-		return this.rgb[0]==c.rgb[0] &&
-			   this.rgb[1]==c.rgb[1] &&
-			   this.rgb[2]==c.rgb[2];
+		return this.rgb[0] == c.rgb[0] && this.rgb[1] == c.rgb[1] && this.rgb[2] == c.rgb[2];
+	}
+	
+	/**
+	 * Function that makes a colour brighter or darker depending on a 'value'
+	 */
+	Color brighter(int value) {
+		int r = Math.max(Math.min(this.getR() + value, Color.MAX), 0);
+		int g = Math.max(Math.min(this.getG() + value, Color.MAX), 0);
+		int b = Math.max(Math.min(this.getB() + value, Color.MAX), 0);
+
+		return new Color(r, g, b);
 	}
 
 }
--- src/ColorImageUtil.java
+++ src/ColorImageUtil.java
@@ -73,11 +73,44 @@ class ColorImageUtil {
 
 		for (int x = 0; x < imgNew.getWidth(); x++)
 			for (int y = 0; y < imgNew.getHeight(); y++) {
-				int rgb = (int)(0.3*img.getColor(x, y).getR() + 0.59*img.getColor(x, y).getG() +0.11*img.getColor(x, y).getB());
+				int rgb = (int) (0.3 * img.getColor(x, y).getR() + 0.59 * img.getColor(x, y).getG()
+						+ 0.11 * img.getColor(x, y).getB());
 				Color grey = new Color(rgb, rgb, rgb);
 				imgNew.setColor(x, y, grey);
 			}
+
 		return imgNew;
+	}
+
+	static ColorImage vignette(ColorImage img, double r) {
+		/**
+		 * Function applies the vignette effect to a copy of an image.
+		 */
+
+		ColorImage imgNew = new ColorImage(img.getWidth(), img.getHeight());
+		
+		double centerX=img.getWidth()/2;
+		double centerY=img.getHeight()/2;
+		
+		for (int x = 0; x < imgNew.getWidth(); x++)
+			for (int y = 0; y < imgNew.getHeight(); y++) {
+				double dist=Math.sqrt((x-centerX)*(x-centerX)+(y-centerY)*(y-centerY));
+				if (dist<=r)
+					imgNew.setColor(x, y, img.getColor(x, y));
+			    else{
+			    	Color vine = img.getColor(x, y).brighter(-(int)(dist-r));
+					imgNew.setColor(x, y, vine);
+				}
+				
+			}
+		
+		return imgNew;
+	}
+
+	static void test_1_5() {
+		ColorImage img = new ColorImage("photo04_2.png");
+		double dist = 40;
+		ColorImage imgNew = vignette(img, dist);
 	}
 
 	static void test_1_4() {
