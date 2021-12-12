f1aa5340328dc8ec8f5887afbe90221a919d284f
aberraxao <diana_milu@live.com.pt>
2021-12-12 00:17:12 +0000

    Format file

--- src/Foto.java
+++ src/Foto.java
@@ -1,9 +1,9 @@
 /**
- * Represents fotos. There are 5 attributes:
- * - foto of type ColorImage
+ * The purpose of the class Foto is to create and manipulate photos with 5 attributes: 
+ * - foto of type ColorImage 
  * - subtitle of type String
  * - creation of type String
- * - x and y of type int to represent the positions in the Pagina
+ * - x, y of type int to represent the positions in the Pagina
  */
 class Foto {
 	private ColorImage foto;
@@ -19,11 +19,11 @@ class Foto {
 		this.x = 0;
 		this.y = 0;
 	}
-	
-	void setFoto(ColorImage foto){
+
+	void setFoto(ColorImage foto) {
 		this.foto = foto;
 	}
-	
+
 	void setSubtitle(String sub) {
 		this.subtitle = sub;
 	}
@@ -36,79 +36,79 @@ class Foto {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	void setX(int x) {
 		this.x = x;
 	}
-	
+
 	void setY(int y) {
 		this.y = y;
 	}
-	
-	ColorImage getFoto(){
+
+	ColorImage getFoto() {
 		return this.foto;
 	}
-	
-	String getSubtitle(){
+
+	String getSubtitle() {
 		return this.subtitle;
 	}
-	
-	String getCreationDate(){
+
+	String getCreationDate() {
 		return this.creationDate;
 	}
-	
-	int getX(){
+
+	int getX() {
 		return this.x;
 	}
-	
-	int getY(){
+
+	int getY() {
 		return this.y;
 	}
-	
+
 	static void test() {
 		ColorImage img = new ColorImage("photo04_2.png");
 		String sub = "Gato com fundo branco.";
 		String cd = "09-12-2021.";
-		
+
 		Foto fotoNew = new Foto(img, sub, cd);
 	}
-	
+
 	static void test_2_1() {
 		ColorImage img = new ColorImage("photo04_2.png");
 		String sub = "Gato com fundo branco.";
 		String cd = "09-12-2021";
-		
+
 		Foto fotoNew = new Foto(img, sub, cd);
-		
+
 		sub = "Gato cinza.";
 		fotoNew.setSubtitle(sub);
 	}
-	
+
 	static void test_2_2() {
 		ColorImage img = new ColorImage("photo04_2.png");
 		String sub = "Gato com fundo branco.";
 		String cd = "09-12-2021.";
-		
+
 		Foto fotoNew = new Foto(img, sub, cd);
-		
+
 		cd = "01-12-2021";
 		fotoNew.setCreationDate(cd);
 	}
-	
+
 	static void test_2_3() {
 		ColorImage img = new ColorImage("photo04_2.png");
 		String sub = "Gato com fundo branco.";
 		String cd = "09-12-2021.";
-		
+
 		Foto fotoNew = new Foto(img, sub, cd);
-		
+
 		int x = 10, y = 20;
 		fotoNew.setPositions(x, y);
-		
-		x=4;
+
+		x = 4;
 		fotoNew.setX(x);
-		
-		y=30;
+
+		y = 30;
 		fotoNew.setY(y);
 	}
 
